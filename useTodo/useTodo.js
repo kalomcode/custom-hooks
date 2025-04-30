f269ec4e66f3eeb1e5f5e33672cfeeec9068ed97
ccalzada <ccalzada@minsait.com>
2025-04-30 18:05:33 +0200

    create folders

--- useTodo/useTodo.js
+++ useTodo/useTodo.js
@@ -1,5 +1,5 @@
-import { useEffect, useReducer, useState } from "react";
-import { todoReducer } from "../08-useReducer/todoReducer";
+import { useEffect, useReducer } from "react";
+import { todoReducer } from "./todoReducer";
 
 export const useTodo = () => {
   const init = () => {
